63d264e1b9583987f5ec06d847f09a1b248f1880
Lotus <416097160@qq.com>
2017-09-10 14:39:43 +0800

    创建订单应用

---
+++ ttsx/detail/models.py
@@ -0,0 +1,19 @@
+#couding=utf-8
+from django.db import models
+
+# Create your models here.
+
+class OrderInfo(models.Model):
+    oid = models.CharField(max_length=20,primary_key=True)
+    user = models.ForeignKey('user.UserInfo')
+    odate = models.DateTimeField(auto_now_add=True)
+    oIsPay = models.BigIntegerField(default=False)
+    ototal = models.DecimalField(max_digits=6,decimal_places=2)
+    oaddress = models.CharField(max_length=150)
+
+
+class OrderDetailInfo(models.Model):
+    goods = models.ForeignKey('goods.goods_goodsinfo')
+    order = models.ForeignKey(OrderInfo)
+    price = models.DecimalField(max_digits=5,decimal_places=2)
+    count = models.IntegerField()
--- ttsx/ttsx/urls.py
+++ ttsx/ttsx/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url('^user/', include('user.urls'))
-
+    url('^user/', include('user.urls')),
+    url('detail/',include('detail.urls')),
 ]
