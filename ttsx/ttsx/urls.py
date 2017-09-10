5375127e2c11c223c24934a334d594f3a34cb99d
kenZQ <kenke35990@163.com>
2017-09-10 11:12:57 +0800

    增加app-user

--- ttsx/ttsx/urls.py
+++ ttsx/ttsx/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url('^', include('index.urls')),
+    url('^user/', include('user.urls'))
+
 ]
