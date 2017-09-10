e2e7d9023264838861d56a6b71154cb2ea3c4582
cloverzsl <1967640341@qq.com>
2017-09-10 18:15:26 +0800

    提交2

--- ttsx/goods/urls.py
+++ ttsx/goods/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import include, url
 from goods import views
 
 urlpatterns = [
-    url('^$',views.index)
+    url('^$',views.index),
+    url('^model01/$',views.model01),
+    url('^model01/$',views.model02),
+    url('^model02/$',views.model03),
+    url('^model03/$',views.model04),
+    url('^model04/$',views.model05),
+    url('^model05/$',views.model06),
+
  ]
--- ttsx/goods/views.py
+++ ttsx/goods/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'goods/index.htm.html')
+    return render(request, 'goods/index.html')
 
-def list(request):
-    value=request.POST.get('s1')
-
+# def list(request):
+#     value=request.POST.get('s1')
+def model01(request):
+    return render(request, 'goods/model01.html')
+def model02(request):
+    return render(request, 'goods/model02.html')
+def model03(request):
+    return render(request, 'goods/model03.html')
+def model04(request):
+    return render(request, 'goods/model04.html')
+def model05(request):
+    return render(request, 'goods/model05.html')
+def model06(request):
+    return render(request, 'goods/model06.html')
