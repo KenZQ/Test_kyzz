f8371d2da6e9a82235a95766b033b02bb23e4992
yhc <577123447@qq.com>
2017-09-11 21:35:08 +0800

    0

--- ttsx/cart/urls.py
+++ ttsx/cart/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^cart/$', views.cart),
+    url('^$', views.cart),
+    url(r'add(\d+)_(\d+)/$',views.add),
+    url(r'^edit(\d+)_(\d+)/$',views.edit),
+    url(r'^delete(\d+)/$',views.delete),
 ]
