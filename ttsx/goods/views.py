c4b556a56d494cdfadfe0d057f3534155d26ebba
yhc <577123447@qq.com>
2017-09-11 12:44:27 +0800

    5

--- ttsx/goods/views.py
+++ ttsx/goods/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'goods/index.htm.html')
+    return render(request, 'goods/index.html')
 
 def list(request):
     value=request.POST.get('s1')
