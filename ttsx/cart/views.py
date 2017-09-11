ca6331dc7d3d89fca5e976e15479d9bf878c31a2
Lotus <416097160@qq.com>
2017-09-11 15:27:57 +0800

    乱了...

--- ttsx/cart/views.py
+++ ttsx/cart/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+import user
+from cart.models import CartInfo
+
+
+
 def cart(request):
-    return render(request, 'cart/cart.html')
+    uid = request.session.get('uid')
+    carts = CartInfo.objects.filter(user_id=uid)
+    context = {
+        'title':'购物车',
+        'name':1,
+        'carts':carts,
+               }
+    return render(request,'cart/cart.html')
+
--- ttsx/ttsx/settings.py
+++ ttsx/ttsx/settings.py
@@ -122,7 +122,7 @@ STATICFILES_DIRS = [
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
-<<<<<<< HEAD
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.163.com'
 EMAIL_PORT = 25
@@ -136,12 +136,12 @@ EMAIL_FROM = 'python<keke35990@163.com>'
 
 
 CELERY_IMPORTS = ('user.task')
-=======
+
 TINYMCE_DEFAULT_CONFIG = {
     'theme': 'advanced',
     'width': 600,
     'height': 400,
 }
->>>>>>> cloverzsl
+
 
 
