194c0c3a08f3b3ed14d627e0c767cb8b7a5b2225
yhc <577123447@qq.com>
2017-09-13 15:01:11 +0800

    10

--- ttsx/cart/models.py
+++ ttsx/cart/models.py
@@ -6,8 +6,5 @@ from django.db import models
 class CartInfo(models.Model):
     user=models.ForeignKey('user.UserInfo')
     goods=models.ForeignKey('goods.GoodsInfo')
-<<<<<<< HEAD
     count=models.IntegerField()
-=======
-    count=models.IntegerField()
->>>>>>> eb56ee4ff6af628628001f5376ec6db14738889d
+
