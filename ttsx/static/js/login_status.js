c4429ce601ae4468ec31501a4c99617feeb0b304
kenZQ <kenke35990@163.com>
2017-09-13 08:33:43 +0800

    1

--- ttsx/static/js/login_status.js
+++ ttsx/static/js/login_status.js
@@ -3,7 +3,7 @@
  */
 $(function () {
     $.get('/user/top_area/', function (data) {
-        if (data.name != '') {
+        if (data.name ) {
             var str1 = '<a href="/user/exit/" style="text-decoration:none; color:#666">&nbsp;&nbsp;退出</a>';
             $('.login_info').empty().prepend('欢迎您：<em>' + data.uname + '</em>' +str1);
             $('.login_btn').css('display', 'none');
